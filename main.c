cf70be14d5ee49898dbc712aa581deb643dd3b2d
Franck GOJARD <frgojard@e3r5p19.clusters.42paris.fr>
2022-11-08 12:24:35 +0100

    o

--- main.c
+++ main.c
@@ -6,15 +6,42 @@
 /*   By: frgojard <frgojard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 09:07:00 by frgojard          #+#    #+#             */
-/*   Updated: 2022/10/25 09:10:56 by frgojard         ###   ########.fr       */
+/*   Updated: 2022/11/08 12:03:07 by frgojard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+
 int	main(int argc, char **argv)
 {
-	if (argc == 5)
+	(void)argv;
+	(void)argc;
+	int id;
+	int n;
+	n = 1;
+	id = fork();
+	wait(NULL);
+	if (id != 0)
 	{
-		
+		n = 6;
+		while(n <= 10)
+		{
+			printf("main %d ", n);
+			n++;
+		}	
 	}
+	if (id == 0)
+	{
+		n = 1;
+		while(n <= 5)
+		{
+			printf("child %d ", n);
+			n++;
+		}	
+	}
+	if (id != 0)
+		printf("\n");
 	return (0);
 }
